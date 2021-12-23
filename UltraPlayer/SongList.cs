21f92008a1a8dfee300944229ea603673152cf5c
Lưu Tiến Phát <luutienphat@gmail.com>
2021-12-23 23:21:32 +0700

    Create class song list

---
+++ UltraPlayer/SongList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraPlayer
+{
+    internal class SongList : List<FileInfo>
+    {
+        public SongList()
+        {
+        }
+
+        public FileInfo getSongByFullname(string fullname)
+        {
+            foreach (var song in this)
+            {
+                if (song.FullName.Equals(fullname)) return song;
+            }
+            return null;
+        }
+
+        public FileInfo getSongByName(string name)
+        {
+            foreach (var song in this)
+            {
+                if (song.FullName.Equals(name)) return song;
+            }
+            return null;
+        }
+    }
+}
